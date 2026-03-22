86a6adef694b13f1afb349e466770a2de09a8620
Johan Dahlin <jdahlin@gmail.com>
2026-03-22 17:40:48 +0100

    LibJS: Box ExpressionKind::TemplateLiteral variant

--- Libraries/LibJS/Rust/src/parser/expressions.rs
+++ Libraries/LibJS/Rust/src/parser/expressions.rs
@@ -2090,10 +2090,10 @@ impl Parser<'_> {
 
         self.expression(
             start,
-            ExpressionKind::TemplateLiteral(TemplateLiteralData {
+            ExpressionKind::TemplateLiteral(Box::new(TemplateLiteralData {
                 expressions,
                 raw_strings,
-            }),
+            })),
         )
     }
 
