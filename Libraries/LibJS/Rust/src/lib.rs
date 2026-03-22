19178701140cfe61c013658a0d2e51ca724da83e
Johan Dahlin <jdahlin@gmail.com>
2026-03-22 19:47:45 +0100

    LibJS: Box StatementKind::Labelled variant

--- Libraries/LibJS/Rust/src/lib.rs
+++ Libraries/LibJS/Rust/src/lib.rs
@@ -2122,8 +2122,8 @@ fn for_each_child_statement(
                 f(&finalizer.inner);
             }
         }
-        StatementKind::Labelled { item, .. } => {
-            f(&item.inner);
+        StatementKind::Labelled(data) => {
+            f(&data.item.inner);
         }
         // Don't recurse into function/class bodies (new var scopes)
         _ => {}
